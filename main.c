7907c1af998275bc1b395435741ff9685885ba6a
sairui-poets <67748283+sairui-bot@users.noreply.github.com>
2023-03-25 13:59:33 +0800

    Add files via upload

---
+++ main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+
+    float a, b;
+    char op;
+
+    scanf("%f %c %f", &a, &op, &b);
+
+    switch (op) {
+
+        case '+':
+            printf("%.2f + %.2f = %.2f", a, b, a + b);
+            break;
+        case '-':
+            printf("%.2f - %.2f = %.2f", a, b, a - b);
+            break;
+        case '*':
+            printf("%.2f * %.2f = %.2f", a, b, a * b);
+            break;
+        case '/':
+            printf("%.2f / %.2f = %.2f", a, b, a / b);
+            break;
+        default:
+            printf("ERROR");
+
+    }
+
+}
